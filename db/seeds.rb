604dec8ea7a3b42bee3110cc6ecdf2c933745476
Michael Pavling <pavling@gmail.com>
2014-04-11 23:41:51 +0100

    adding seeds for metrics

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
 u = User.new(email: 'demo_admin@example.com', password: 'password', password_confirmation: 'password')
 u.role = :admin
 u.save!
+
+Metric.create! measure: "The instructional team is organised and prepared", active: true
+Metric.create! measure: "The pace of the class is just right", active: true
+Metric.create! measure: "I am enjoying this lesson", active: true
+Metric.create! measure: "I think the other students are enjoying this class", active: true
+Metric.create! measure: "My general mood is good", active: true
