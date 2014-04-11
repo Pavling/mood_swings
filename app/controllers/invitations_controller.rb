d2dc5ee9d831d4e0e3b3238083b980e2f1ec1668
Michael Pavling <pavling@gmail.com>
2014-04-11 18:40:50 +0100

    DRYing invitations controller, and incorportating authorization checks

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,14 +1,11 @@
 class InvitationsController < Devise::InvitationsController
 
-  # overloading the invitations process to populate an instance variable for the cohorts that are available to invite people to
-  def new
-    @cohorts = current_user.accessible_cohorts.current_and_future
-    super
-  end
+  before_filter :load_and_authorize, only: [:new, :create]
 
-  def create
-    @cohorts = current_user.accessible_cohorts.current_and_future
-    super
+  private
+  def load_and_authorize
+    authorize! :invite, User
+    @cohorts = current_user.invitable_cohorts
   end
 
 end
