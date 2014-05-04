8cb5e312763beefce98e87d77e559b26cb406951
Michael Pavling <pavling@gmail.com>
2014-05-04 12:25:08 +0100

    altering ability file a little - for legibility and belt&braces

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,36 +4,60 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
 
-    can :create, AnswerSet
+    can :create, AnswerSet if user.persisted?
 
     can :read, AnswerSet do |answer_set|
       answer_set.user == user
     end
 
-    case
-      when user.admin?
-        can :manage, :all
+    if user.admin?
+      can :manage, :all
+    else
 
-        cannot :destroy, Campus do |campus|
-          campus.cohorts.any?
-        end 
+      can :granularity_by_campus, AnswerSet if user.campus_admin?
+      can :granularity_by_cohort, AnswerSet if user.cohort_admin? || user.campus_admin?
 
-      when user.cohort_admin? || user.campus_admin?
-        if user.campus_admin?
-          can :granularity_by_campus, AnswerSet
-          can :manage, :cohort do |cohort|
-            user.administered_cohorts.include?(cohort)
-          end
-        end
+      can :manage, :campus do |campus|
+        user.administered_campuses.include?(campus)
+      end
 
-        can :invite, User if user.invitable_cohorts.any?
+      can :manage, :cohort do |cohort|
+        user.administered_cohorts.include?(cohort)
+      end
 
-        can :granularity_by_cohort, AnswerSet
+      can :read, Cohort do |cohort|
+        user.accessible_cohorts.include?(cohort)
+      end
 
-        can :read, Cohort do |cohort|
-          user.accessible_cohorts.include?(cohort)
-        end
+      can :invite, User if user.invitable_cohorts.any?
+
+      can :manage, User do |target_user|
+        user.accessible_users.include?(target_user)
+      end
+
+      cannot :alter_email, User
 
     end
+
+    cannot :destroy, Answer
+
+    cannot :destroy, AnswerSet
+
+    cannot :destroy, Campus do |campus|
+      campus.cohorts.any?
+    end
+
+    cannot :destroy, Cohort do |cohort|
+      cohort.answer_sets.any?
+    end
+
+    cannot :destroy, Metric do |metric|
+      metric.answers.any?
+    end
+
+    cannot :destroy, User do |target_user|
+      target_user.answer_sets.any?
+    end
+
   end
 end
