951d973d8950c06b08c39856ca92dc342f76263c
Michael Pavling <pavling@gmail.com>
2014-05-14 22:07:13 +0100

    tweaking factory syntax

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,7 +1,10 @@
 FactoryGirl.define do
-  factory :answer do |f|
-    f.value { (1..5).to_a.sample }
-    f.comments { Faker::Lorem.sentence }
+  factory :answer do
+    value { (1..5).to_a.sample }
     association :metric, factory: :metric, strategy: :create
+
+    factory :answer_with_comments do
+      comments { Faker::Lorem.sentence }
+    end
   end
 end
--- spec/factories/metrics.rb
+++ spec/factories/metrics.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
-  factory :metric do |f|
-    f.active true
-    f.measure { Faker::Lorem.sentence }
+  factory :metric do
+    active true
+    measure { Faker::Lorem.sentence }
     
     factory :metric_with_answers do
       after(:create) do |metric, evaluator|
