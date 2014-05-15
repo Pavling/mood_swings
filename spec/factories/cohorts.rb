f873b399d909b332622487b71eb704ddb7693036
Michael Pavling <pavling@gmail.com>
2014-05-15 09:51:17 +0100

    tweaking fake name for cohorts

--- spec/factories/cohorts.rb
+++ spec/factories/cohorts.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :cohort do
-    name { Faker::Address.city }
+    name { Faker::Team.name }
     start_on { (3.months.ago.to_date..Date.yesterday).to_a.sample }
     end_on { (Date.tomorrow..3.months.since.to_date).to_a.sample }
   end
