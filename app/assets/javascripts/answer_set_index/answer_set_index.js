64999cdb376b13be5cff0909ee652e7e52a27c06
alexpchin <alex@womapp.com>
2014-10-16 10:32:03 +0100

    Resize chart with window size in answer_set_index.js

--- app/assets/javascripts/answer_set_index/answer_set_index.js
+++ app/assets/javascripts/answer_set_index/answer_set_index.js
@@ -1,23 +1,27 @@
-$(function(){
+function setup_chart() {
+  var chart = new Morris.Line({
+    element: 'myChart',
+    ymax: 5,
+    ymin: 1,
+    smooth: false,
+    data: $('#line-chart').data('data'),
+    labels: $('#line-chart').data('labels'),
+    xkey: 'timestamp',
+    ykeys: $('#line-chart').data('keys'),
+    xLabels: $('#line-chart').data('x-labels'),
+    moveHover: false,
+    hoverContainer: $('#hoverContainer'),
+    compactLegend: true
+  });
 
-  function setup_chart() {
-    new Morris.Line({
-      element: 'myChart',
-      ymax: 5,
-      ymin: 1,
-      smooth: false,
-      data: $('#line-chart').data('data'),
-      labels: $('#line-chart').data('labels'),
-      xkey: 'timestamp',
-      ykeys: $('#line-chart').data('keys'),
-      xLabels: $('#line-chart').data('x-labels'),
-      moveHover: false,
-      hoverContainer: $('#hoverContainer'),
-      compactLegend: true
+  if ($('#myChart').length) {
+    $(window).on('resize', function(){
+      $('svg').css("width", "100%"); // Improves redraw
+      chart.redraw();
     });
+  }
+};
 
-  };
-
+$(function(){
   setup_chart();
-
 });
